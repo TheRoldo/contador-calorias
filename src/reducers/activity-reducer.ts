edd881039be0c6d966f199a83544d142ccb77b56
unknown <nroldangaviria@gmail.com>
2024-09-24 20:44:16 -0500

    Primer commit

---
+++ src/reducers/activity-reducer.ts
@@ -0,0 +1,24 @@
+import { Activity } from "../types"
+
+export type ActivityActions = 
+    { type: 'save-activity', payload: {newActivity : Activity} }
+
+type ActivityState = {
+    activities : Activity[]
+}
+
+export const inicialState : ActivityState = {
+    activities: []
+}   
+
+export const activityReducer = (state: ActivityState = inicialState, action: ActivityActions) => {
+    if(action.type === 'save-activity') {
+        //Este codigo maneja la logica para actualizar el state
+
+        return {
+            ...state,
+            activities: [...state.activities, action.payload.newActivity]
+        }
+    }
+    return state
+}
